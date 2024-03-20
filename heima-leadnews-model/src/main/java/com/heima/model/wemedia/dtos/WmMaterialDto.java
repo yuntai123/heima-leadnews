670cecc6b43060142d1e0cef4377491126b4656e
RowAn <1741832353@qq.com>
2024-03-20 17:47:15 +0800

    day3

---
+++ heima-leadnews-model/src/main/java/com/heima/model/wemedia/dtos/WmMaterialDto.java
@@ -0,0 +1,14 @@
+package com.heima.model.wemedia.dtos;
+
+import com.heima.model.common.dtos.PageRequestDto;
+import lombok.Data;
+
+@Data
+public class WmMaterialDto extends PageRequestDto {
+
+    /**
+     * 1收藏
+     * 0未收藏
+     */
+    private Short isCollection;
+}
---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/interceptor/WmTokenInterceptor.java
@@ -0,0 +1,50 @@
+package com.heima.wemedia.interceptor;
+
+import com.heima.model.wemedia.pojos.WmUser;
+import com.heima.utils.thread.WmThreadLocalUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
+
+@Slf4j
+public class WmTokenInterceptor implements HandlerInterceptor {
+
+    /**
+     * 得到header中的用户信息，并且存入到当前线程中
+     * @param request
+     * @param response
+     * @param handler
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String userId = request.getHeader("userId");
+        Optional<String> optional = Optional.ofNullable(userId);
+        if (optional.isPresent()) {
+            WmUser wmUser=new WmUser();
+            wmUser.setId(Integer.valueOf(userId));
+            WmThreadLocalUtil.setUser(wmUser);
+            log.info("wmTokenFilter设置用户信息到threadlocal中...");
+        }
+        return true;
+    }
+
+    /**
+     * 清理线程中的数据
+     * @param request
+     * @param response
+     * @param handler
+     * @param modelAndView
+     * @throws Exception
+     */
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        log.info("清理threadlocal...");
+        WmThreadLocalUtil.clear();
+    }
+}
